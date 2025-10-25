3f1cceb8898282f9d8f8e319560b30e00a9289f6
Roy Illachura <rillachura@unsa.edu.pe>
2025-10-24 22:12:45 -0500

    Se termino el ejercicio 11

---
+++ ejercicio11.js
@@ -0,0 +1,16 @@
+
+let num = prompt(
+    "ES NUMERO DE ARMSTRONG??\n"+
+    "Ingrese un numero:"
+);
+let suma=0;
+for (let i=0; i<num.length; i++){
+    let cifra=parseInt(num.charAt(i));
+    suma+=Math.pow(cifra,num.length);
+}
+if (suma==num){
+    alert(`${num} es un numero de Armstong`);
+}
+else {
+    alert(`${num} no es un numero de Armstong`);
+}
