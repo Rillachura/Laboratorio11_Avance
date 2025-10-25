1adcb35ef904b0dd4853c287423380bde457b687
Roy Illachura <rillachura@unsa.edu.pe>
2025-10-24 20:31:57 -0500

    Se termino el ejercicio 07

--- ejercicio07.js
+++ ejercicio07.js
@@ -1,6 +1,9 @@
 let monto = parseInt(prompt("Que monoto desea retirar (min 10): "));
 let billetes100 = parseInt(monto/100);
-let billetes50 = parseInt((monto-(billetes100*100)))/50;
-let billetes20 = parseInt((monto-((billetes100*100)+(billetes50*50))))/20;
-let billetes10 = parseInt((monto-((billetes100*100)+(billetes50*50)+(billetes20*20))))/10;
-alert(`Billetes de 100: ${billetes100}\nBilletes de 50: ${billetes50}\nBilletes de 20: ${billetes20}\nBilletes de 10: ${billetes10}`);
+let sobra1 = monto%100;
+let billetes50 = parseInt(sobra1/50);
+let sobra2 = sobra1%50;
+let billetes20 = parseInt(sobra2/20);
+let sobra3 = sobra2%20;
+let billetes10 = parseInt(sobra3/10);
+alert(`Monto a retirar ${monto}\n${billetes100} Billetes de 100\n${billetes50} Billetes de 50\n${billetes20} Billetes de 20\n${billetes10} Billetes de 10`);
