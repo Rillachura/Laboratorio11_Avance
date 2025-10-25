e33fb6f333c26c5dac0e0222a29f19d10cdd06fb
Roy Illachura <rillachura@unsa.edu.pe>
2025-10-25 12:29:23 -0500

    Se termino el ejercicio 12

--- ejercicio12.js
+++ ejercicio12.js
@@ -11,7 +11,9 @@ do {
   ));
   if (opcion == 1) {
     let n = parseInt(prompt("Cuantas notas desea ingresar?"));
+    let notas=[];
     let suma=0, maximo=0, minimo=20, contPares=0, contImpares=0;
+
     for (let i=1; i<=n; i++) {
         let nota = parseFloat(prompt(`Ingrese nota ${i}`));
         notas[i-1] = nota;
@@ -25,22 +27,52 @@ do {
     let promedio = suma/n;
     let porEncima = 0;
     for (let i=0; i<n; i++){
-        if (nota[i]>promedio) porEncima++;
+        if (notas[i]>promedio) porEncima++;
     }
-
-    let opcion2 = parseInt(prompt(
-        "===== MENU DE ESTADISTICAS =====\n" +
+    let opcion2;
+    do {
+      opcion2 = parseInt(prompt(
+        "===== MENÚ DE ESTADISTICAS =====\n" +
         "1. Promedio\n" +
         "2. Valor maximo\n" +
         "3. Valor minimo\n" +
         "4. Cantidad de pares e impares\n" +
-        "5. Cuantos estan por encima del promedio\n"
-    ));
+        "5. Cuantos estan por encima del promedio\n" +
+        "6. Volver al menu principal\n" +
+        "===========================\n" +
+        "Elija una opción:"
+      ));
 
-    if (opcion2 == 1) alert(`El promedio de notas es ${promedio}`);
-    else if (opcion2 == 2) alert(`La nota maxima es ${maximo}`);
-    else if (opcion2 == 3) alert(`La nota minima es ${minimo}`);
-    else if (opcion2 == 4) alert(`Notas pares ${contPares}\nNotas impares ${contImpares}`);
-    else if (opcion2 == 5) alert(`${porEncima} Estan por encima del promedio`)
+      if (opcion2 == 1) alert(`El promedio de notas es: ${promedio}`);
+      else if (opcion2 == 2) alert(`La nota maxima es: ${maximo}`);
+      else if (opcion2 == 3) alert(`La nota minima es: ${minimo}`);
+      else if (opcion2 == 4) alert(`Notas pares: ${contPares}\nNotas impares: ${contImpares}`);
+      else if (opcion2 == 5) alert(`${porEncima} notas estan por encima del promedio`);
+      else if (opcion2 == 6) alert(`Volviendo al menu principal...`);
+      else alert(`Ingrese opcion valida`);
+    } while (opcion2 != 6);
   }
-} while (opcion !== 9);
+  else if (opcion == 2){
+    let inicio = parseInt(prompt("Ingrese el inicio del rango:"));
+    let fin = parseInt(prompt("Ingrese el final del rango:"));
+    let pares = 0, impares = 0;
+
+    for (let i=inicio; i<=fin; i++) {
+      if (i%2 == 0) pares++;
+      else impares++;
+    }
+    alert(`Entre ${inicio} y ${fin}: Hay ${pares} pares y ${impares} impares.`);
+  }
+  else if (opcion == 3){
+    let numero = parseInt(prompt("Ingrese numero para ver su tabla: "));
+    let tabla="";
+    for (let i=0; i<12; i++) {
+      tabla+=(`${numero} x ${i+1} = ${numero*(i+1)}\n`);
+    }
+    alert(tabla);
+  }
+  else if (opcion == 4){
+    alert("Saliendo del programa...");
+  }
+  else alert("Opcion invalida.")
+} while (opcion != 4);
